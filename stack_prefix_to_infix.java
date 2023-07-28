d6641f5afc8cff0c41402acea401549f6c1280d8
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2023-07-28 18:28:21 +0530

    Add files via upload

---
+++ stack_prefix_to_infix.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class stack_prefix_to_infix {
+    public static int equation(String str) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            int ascii = (int) ch;
+
+            if (ascii >= 48 && ascii <= 57)// '0'-->48 & '9'-->57 is ascii
+            {
+                st.push(ascii - 48);
+            } else {
+                int v1 = st.pop();
+                int v2 = st.pop();
+                if (ch == '+')
+                    st.push(v1 + v2);
+                if (ch == '-')
+                    st.push(v1 - v2);
+                if (ch == '*')
+                    st.push(v1 * v2);
+                if (ch == '/')
+                    st.push(v1 / v2);
+            }
+        }
+        return st.peek();
+    }
+
+    public static void main(String[] args) {
+        String str = "-9/*+5346";
+
+        System.out.println(equation(str));
+
+    }
+}
